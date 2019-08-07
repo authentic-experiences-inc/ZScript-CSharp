211824e3a62cd1e24b946d94e6b7055d2f85aa94
Zac North <znorth@gmail.com>
2019-08-07 01:33:34 -0400

    Initial .NET Core project

---
+++ Interpreter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AuthenticExperiences.ZScriptCSharp
+{
+    public class Interpreter
+    {
+        public void Interpret(string Input)
+        {
+            
+        }
+    }
+}
